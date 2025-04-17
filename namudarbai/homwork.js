4838e6e51afad7b487cf040508b995a662e65df2
AKURA333 <ricardasakura@gmail.com>
2025-04-17 09:52:39 +0300

    Update homwork.js

--- namudarbai/homwork.js
+++ namudarbai/homwork.js
@@ -10,21 +10,23 @@ let profesija='testuotojas';
 console.log(vardas);
 console.log(miestas);
 console.log(profesija);
-
-
-const sarasas1 =[1,2,3,4,5];
+let num1 =10;const sarasas1 =[1,2,3,4,5];
 console.log(sarasas1);
 
-const sarasas2 =[10,20,30,40.50];
+const sarasas2 =[10,20,30,40,60];
 console.log(sarasas2);
 
-const sarasas3 =[100,200,300,400,500];
+const sarasas3 = [100,200,300,400,500];
 console.log(sarasas3);
-let sar1 =("bananas","kriauses","obuoliai","slyva","apelsinas");
-console.log("pirmas sarasas");
-let sar2=("geltonas","orandzines","raudoni","melynas","gelsvos");
-console.log("antras sarasas");
-let sar3=("maiselyje","kibire","kiseneje","stalciuje","virtuveje");
-console.log("trecias sarasas");
+let sar1 = ['bananas','kriauses','obuoliai','slyva','apelsinas'];
+console.log(sar1);
+let sar2 =['geltonas','orandzines','raudoni','melynas','gelsvos'];
+console.log(sar2);
+let sar3=['maiselyje','kibire','kiseneje','stalciuje','virtuveje'];
+console.log(sar3);
+let num2 =20;
+let num3=30;
+
+
 
 
