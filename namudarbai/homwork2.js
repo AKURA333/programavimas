33717f2f27a579bb8fd5a6b1b2dcdf8461aa0490
AKURA333 <ricardasakura@gmail.com>
2025-04-17 09:49:07 +0300

    Update homwork2.js

--- namudarbai/homwork2.js
+++ namudarbai/homwork2.js
@@ -3,10 +3,10 @@ console.log(a);
 let b=5.5
 console.log(a+b);
 const zodis1="labas"; 
-console.log("labas");
+console.log("zodis1");
 const zodis2="kaip";
-console.log("kaip");
+console.log("zodis2");
 const zodis3="Sekasi";
-console.log("sekasi");
+console.log("zodis3");
 
 console.log("zodis1","zodis2","zodis3");
